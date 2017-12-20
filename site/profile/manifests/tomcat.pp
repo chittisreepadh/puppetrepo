eae207d5c7c01d5e7284fbfcbdba3fb0b5955f41
bandaru <bandaru@pemaster.eidikointernal.com>
2017-12-20 17:00:51 -0500

    test3

--- site/profile/manifests/tomcat.pp
+++ site/profile/manifests/tomcat.pp
@@ -1,4 +1,4 @@
-class profile::java::tomcat {
+class profile::tomcat {
 tomcat::install { '/opt/tomcat' :
 source_url => 'https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.0.M1/bin/apache-tomcat-9.0.0.M1.tar.gz',
 }
